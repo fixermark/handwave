ee664fec5c452982470a9ecb01388795e49db858
Mark T. Tomczak <iam@fixermark.com>
2020-08-18 05:47:51 +0100

    Added support for processing song list and initial notes. Next to do: implement support for individual un-chorded notes

--- song.scm
+++ song.scm
@@ -12,7 +12,7 @@
 (define *errors* (make-gvector))
 (define *output* (make-gvector))
 (define *current-note-bindings* (make-hash))
-(define *initial-note-bindings* (make-hash))
+(define *initial-note-bindings* (make-gvector))
 (define *note-name->bytes* (make-hash))
 
 (define *note-names* '("c" "d" "e" "f" "g" "a" "b"))
@@ -34,11 +34,11 @@
 	 [sharping #f])
     (for ([period periods])
 	 (if sharping
-	     (let ([sharp-name (format "~a#~a" (car remaining-notes) cur-octave)]
-		   [flat-name (format "~ab~a" (cadr remaining-notes) cur-octave)])
+	     (let ([sharp-name (string->symbol (format "~a#~a" (car remaining-notes) cur-octave))]
+		   [flat-name (string->symbol (format "~ab~a" (cadr remaining-notes) cur-octave))])
 	       (hash-set! *note-name->bytes* sharp-name period)
 	       (hash-set! *note-name->bytes* flat-name period))
-	     (hash-set! *note-name->bytes* (format "~a~a" (car remaining-notes) cur-octave) period))
+	     (hash-set! *note-name->bytes* (string->symbol (format "~a~a" (car remaining-notes) cur-octave)) period))
 	 (if (and (not sharping) (member (car remaining-notes) *sharpable-notes*))
 	     (set! sharping #t)
 	     (begin
@@ -62,9 +62,51 @@
 (define (parse-file fname)
   (printf "Parsing happens here\n")
   (make-semitone-period-list)
-  (for ([key (hash-keys *note-name->bytes*)])
-       (gvector-add! *output* (format "~a: #x~x" key (hash-ref *note-name->bytes* key)))))
+  (let ([f (open-input-file fname)])
+    (port-count-lines! f)
+    (let ([top-level-form (read-syntax fname f)])
+      (process-song top-level-form))
+    (close-input-port f)))
 
+(define (process-song song-form)
+  (let ([inner (syntax-e song-form)])
+    (cond [(< (length inner) 2) (error 'process-song "list ~a must have at least 2 elements" inner)])
+    (let ([song-sym (syntax-e (car inner))]
+	  [initial-assignments (syntax->list (cadr inner))]
+	  [commands (cddr inner)])
+      (cond [(not (eq? song-sym 'song)) (error 'process-song "list starting with ~a is not a song" song-sym)]
+	    [(not initial-assignments) (error 'process-song "initial-assignments list ~a is not a list" (cadr inner))]
+	    [(not (eq? (length initial-assignments) 16)) (error 'process-song "initial-assignments list ~a must have length 16, is length ~a"
+							       (cadr inner)
+							       (length initial-assignments))])
+      (process-initial-assignments initial-assignments)
+      (process-commands commands))))
+
+;; stores initial assignments to current and initial note bindings
+;; initial-assignments is a list of syntax objects of initial note names
+(define (process-initial-assignments initial-assignments)
+  (let ([i 0])
+    (for ([assignment initial-assignments])
+	 (let ([note (syntax-e assignment)])
+	   (cond [(not (hash-has-key? *note-name->bytes* note)) (error 'process-initial-assignments "Note ~a does not exist" assignment)])
+	   (hash-set! *current-note-bindings* i note)
+	   (gvector-add! *initial-note-bindings* note)
+	   (set! i (add1 i))))))
+
+(define (process-commands commands)
+  ;; todo
+  #f)
+
+(define (note-name-to-freq note-name)
+  (cond [(not (hash-has-key? *note-name->bytes* note-name)) (error 'note-name-to-freq "~a is not a note" note-name)]
+	[else (hash-ref *note-name->bytes* note-name)]))
+
+(define (note-id-to-freq note-id)
+  (cond [(or (< note-id 0) (> note-id 15)) (error 'note-id-to-freq "Note id ~a must be between 0 and 15" note-id)]
+	[else (note-name-to-freq (hash-ref *current-note-bindings* note-id))]))
+
+(define (byte->hi-low-string byte)
+  (format "#x~x #x~x" (bitwise-and #xff (arithmetic-shift byte -8)) (bitwise-and #xff byte)))
 
 (define (emit-output fname)
   (let ([f (open-output-file fname #:exists 'replace)])
@@ -73,7 +115,15 @@
 	 (newline f))
     (close-output-port f)))
 
-(define (emit-notes fname) #f)
+(define (emit-notes fname)
+  (let ([f (open-output-file fname #:exists 'replace)])
+    (write-string "(bytes\n" f)
+    (for [(elem *initial-note-bindings*)]
+	 (write-string (byte->hi-low-string (note-name-to-freq elem)) f)
+	 (newline f))
+    (write-string ")" f)
+    (close-output-port f)))
+
 
 ;;;; main
 (define set-out (make-parameter #f))
